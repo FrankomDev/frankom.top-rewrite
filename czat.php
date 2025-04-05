d2aa6c95bf57579677464a54c0aa2e330332b252
FrankomDev <vasylopozdro@gmail.com>
2025-04-05 17:04:18 +0200

    added chat and new images

---
+++ czat.php
@@ -0,0 +1,15 @@
+<?php
+include 'header.html';
+echo '<center>
+            <br>
+            <p class="h3 font-monospace fw-bold">Czat</p>
+            <br>
+            <p class="font-monospace fw-bold">Pozostań w kontakcie!</p>
+            <p class="font-monospace fw-bold">Nasza sieć oferuje usługe czatu tekstowego!</p>
+            <br>
+            <strong><p class="h5 font-monospace">IRC:</strong> irc.frankom.top:6697</p>
+            <br>
+            <img src="gifs/e-mail.gif">
+            </center>';
+include 'footer.html';
+?>
